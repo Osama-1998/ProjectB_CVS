283d4f7c04205a85f7df7c50b816ae0151b3285b
osama-1998 <osama.abboud@campus.technion.ac.il>
2022-01-21 00:31:53 +0200

    Mild changes to Image_Classification_Model.py Next: Implement train method and add an interface for desired Image input

--- modeling/Image_Classification_Model.py
+++ modeling/Image_Classification_Model.py
@@ -1,38 +1,28 @@
-# importing the libraries
-import pandas as pd
-import numpy as np
-
-# for reading and displaying images
-from skimage.io import imread
-import matplotlib.pyplot as plt
-
-# for creating validation set
-from sklearn.model_selection import train_test_split
-
-# for evaluating the model
-from sklearn.metrics import accuracy_score
-from tqdm import tqdm
-
-# PyTorch libraries and modules
-import torch
-from torch.autograd import Variable
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
-from torch.optim import Adam, SGD
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import MaxPool2d
+from torch.nn import ReLU
+from torch.nn import BatchNorm2d
+from torch.nn import Sequential
+from torch.nn import LogSoftmax
+from torch import flatten
 
 
-
-class Net(Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        #fist layer
-        layer1 =  Conv2d(16, 8, kernel_size=5, stride=1, padding=1)
-        layer1.append( BatchNorm2d(4))
+class LeNet(Module):
+    def __init__(self, numChannels, classes):
+        # call the parent constructor
+        super(LeNet, self).__init__()
+        # initialize first set of CONV => RELU => POOL layers
+        # fist layer
+        layer1 = Conv2d(16, 8, kernel_size=5, stride=1, padding=1)
+        layer1.append(BatchNorm2d(4))
         layer1.append(ReLU(inplace=True))
         layer1.append(MaxPool2d(kernel_size=2, stride=2))
 
-        #second layer
-        layer2 =  Conv2d(8, 8, kernel_size=5, stride=1, padding=1)
-        layer2.append( BatchNorm2d(4))
+        # second layer
+        layer2 = Conv2d(8, 8, kernel_size=5, stride=1, padding=1)
+        layer2.append(BatchNorm2d(4))
         layer2.append(ReLU(inplace=True))
         layer2.append(MaxPool2d(kernel_size=2, stride=2))
 
@@ -54,32 +44,26 @@ class Net(Module):
         layer5.append(ReLU(inplace=True))
         layer5.append(MaxPool2d(kernel_size=2, stride=2))
 
-        ((W - K + 2P) / S) + 1)
-        Here
-        W = Input
-        size
-        K = Filter
-        size
-        S = Stride
-        P = Padding
-        linear_layer = Linear(4 * 7 * 7, 10);
-
+        # ((W - K + 2P) / S) + 1) Where W = Input size  K = Filter size  S = Stride  P = Padding
+        # TODO Osama: Calculate correct dimentions for Linear layer input
+        linear_layer = Linear(20, 3)
 
         self.cnn_layers = Sequential(
-            # Defining another 2D convolution layer
-            Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
-            BatchNorm2d(4),
-            ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=2),
+            layer1, layer2, layer3, layer4, layer5
         )
 
         self.linear_layers = Sequential(
-            Linear(4 * 7 * 7, 10)
+            linear_layer
         )
+        self.logSoftmax = LogSoftmax(dim=1)
 
-    # Defining the forward pass
     def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
+            # pass the input through our first set of CONV => RELU =>
+            # POOL layers
+            x = self.cnn_layers(x)
+            x = flatten(x, 1)
+            x = self.linear_layers(x)
+            # pass the output to our softmax classifier to get our output predictions
+            output = self.logSoftmax(x)
+            # return the output predictions
+            return output
